e84876f69c4a50822ef19fff0008eee9c442d868
masa kunikata <masa-kunikata@users.noreply.github.com>
2025-05-16 13:36:22 +0000

    add pwa settings.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import nightwatchPlugin from 'vite-plugin-nightwatch'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,29 @@ export default defineConfig({
     vue(),
     vuetify({ autoImport: true }),
     nightwatchPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Real Book Viewer',
+        short_name: 'RealBook',
+        start_url: '/realbooks_index/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#3f51b5',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
